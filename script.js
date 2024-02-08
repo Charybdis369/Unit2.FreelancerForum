568c815ec34080d68c1c8696503bca196cf1ac4b
richard <ricstewa41@cox.net>
2024-02-08 13:33:35 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const freeLancers = {
+    name: 'Alice',
+    occupation: 'Writer',
+    startingPrice: 30,
+
+    name: 'Bob',
+    occupation: 'Teacher',
+    startingPrice: 50,
+
+    name: 'Carol',
+    occupation: 'Programmer',
+    startingPrice: 70
+
+}
+
+const addFreeLancerInterval = setInterval(addFreeLancer, 2000)
+
+function render() {
+
+}
+
+function addFreeLancer() {
+
+}
